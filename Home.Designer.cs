920e0e2337c8c8cdcab58a318727ca8ba6df4d36
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-06-07 14:52:40 +0700

    add & change: menambahkan form home, mengubah username menjadi NIP

---
+++ Home.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace E_Raport_SMA
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            welcomeLabel = new Label();
+            inputNilaiButton = new Button();
+            kelasButton = new Button();
+            SuspendLayout();
+            // 
+            // welcomeLabel
+            // 
+            welcomeLabel.AutoSize = true;
+            welcomeLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            welcomeLabel.Location = new Point(311, 82);
+            welcomeLabel.Name = "welcomeLabel";
+            welcomeLabel.Size = new Size(183, 32);
+            welcomeLabel.TabIndex = 0;
+            welcomeLabel.Text = "Selamat Datang";
+            // 
+            // inputNilaiButton
+            // 
+            inputNilaiButton.Location = new Point(224, 247);
+            inputNilaiButton.Name = "inputNilaiButton";
+            inputNilaiButton.Size = new Size(112, 34);
+            inputNilaiButton.TabIndex = 1;
+            inputNilaiButton.Text = "Input Nilai";
+            inputNilaiButton.UseVisualStyleBackColor = true;
+            // 
+            // kelasButton
+            // 
+            kelasButton.Location = new Point(500, 247);
+            kelasButton.Name = "kelasButton";
+            kelasButton.Size = new Size(112, 34);
+            kelasButton.TabIndex = 2;
+            kelasButton.Text = "Lihat Kelas";
+            kelasButton.UseVisualStyleBackColor = true;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(kelasButton);
+            Controls.Add(inputNilaiButton);
+            Controls.Add(welcomeLabel);
+            Name = "Home";
+            Text = "Home";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label welcomeLabel;
+        private Button inputNilaiButton;
+        private Button kelasButton;
+    }
+}
--- Login.Designer.cs
+++ Login.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            label2 = new Label();
+            NIPLabel = new Label();
             label3 = new Label();
-            inputUsn = new TextBox();
+            inputNIP = new TextBox();
             inputPassword = new TextBox();
             btnLogin = new Button();
             SuspendLayout();
@@ -40,47 +40,52 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(124, 43);
+            label1.Location = new Point(177, 72);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(189, 30);
+            label1.Size = new Size(288, 45);
             label1.TabIndex = 0;
             label1.Text = "Login to E-Raport";
             label1.Click += label1_Click;
             // 
-            // label2
+            // NIPLabel
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(72, 100);
-            label2.Name = "label2";
-            label2.Size = new Size(63, 13);
-            label2.TabIndex = 1;
-            label2.Text = "Username";
+            NIPLabel.AutoSize = true;
+            NIPLabel.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            NIPLabel.Location = new Point(103, 167);
+            NIPLabel.Margin = new Padding(4, 0, 4, 0);
+            NIPLabel.Name = "NIPLabel";
+            NIPLabel.Size = new Size(39, 20);
+            NIPLabel.TabIndex = 1;
+            NIPLabel.Text = "NIP";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(74, 143);
+            label3.Location = new Point(106, 238);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(61, 13);
+            label3.Size = new Size(91, 20);
             label3.TabIndex = 2;
             label3.Text = "Password";
             // 
-            // inputUsn
+            // inputNIP
             // 
-            inputUsn.Location = new Point(145, 96);
-            inputUsn.Name = "inputUsn";
-            inputUsn.PlaceholderText = "Masukkan Username";
-            inputUsn.Size = new Size(161, 23);
-            inputUsn.TabIndex = 3;
+            inputNIP.Location = new Point(207, 160);
+            inputNIP.Margin = new Padding(4, 5, 4, 5);
+            inputNIP.Name = "inputNIP";
+            inputNIP.PlaceholderText = "Masukkan NIP";
+            inputNIP.Size = new Size(228, 31);
+            inputNIP.TabIndex = 3;
             // 
             // inputPassword
             // 
-            inputPassword.Location = new Point(145, 139);
+            inputPassword.Location = new Point(207, 232);
+            inputPassword.Margin = new Padding(4, 5, 4, 5);
             inputPassword.Name = "inputPassword";
             inputPassword.PlaceholderText = "Masukkan Password";
-            inputPassword.Size = new Size(161, 23);
+            inputPassword.Size = new Size(228, 31);
             inputPassword.TabIndex = 4;
             // 
             // btnLogin
@@ -90,9 +95,10 @@
             btnLogin.CausesValidation = false;
             btnLogin.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnLogin.ForeColor = SystemColors.ControlText;
-            btnLogin.Location = new Point(153, 194);
+            btnLogin.Location = new Point(219, 323);
+            btnLogin.Margin = new Padding(4, 5, 4, 5);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(133, 29);
+            btnLogin.Size = new Size(190, 48);
             btnLogin.TabIndex = 5;
             btnLogin.Text = "Login";
             btnLogin.UseVisualStyleBackColor = false;
@@ -100,16 +106,17 @@
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(440, 298);
+            ClientSize = new Size(629, 497);
             Controls.Add(btnLogin);
             Controls.Add(inputPassword);
-            Controls.Add(inputUsn);
+            Controls.Add(inputNIP);
             Controls.Add(label3);
-            Controls.Add(label2);
+            Controls.Add(NIPLabel);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Login";
             Text = "Login";
             ResumeLayout(false);
@@ -119,9 +126,9 @@
         #endregion
 
         private Label label1;
-        private Label label2;
+        private Label NIPLabel;
         private Label label3;
-        private TextBox inputUsn;
+        private TextBox inputNIP;
         private TextBox inputPassword;
         private Button btnLogin;
     }
